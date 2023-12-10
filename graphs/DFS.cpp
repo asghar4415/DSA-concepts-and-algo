2087beef8145903bd1129ed9854d6de66dd8f0b3
Asghar Ali <asgharali.224415@gmail.com>
2023-12-10 22:12:11 +0500

    updated

--- graphs/DFS.cpp
+++ graphs/DFS.cpp
@@ -1,7 +1,63 @@
 #include <iostream>
-#include <vector>
-#include <stack>
+#include <map>
+#include <list>
+
+
 using namespace std;
 
-// DFS uses Stack data structure
-// It is a recursive algorithm that uses the idea of backtracking. 
+// Graph class represents a directed graph
+// using adjacency list representation
+class Graph {
+public:
+	map<int, bool> visited;
+	map<int, list<int> > adj;
+
+	// Function to add an edge to graph
+	void addEdge(int v, int w);
+
+	// DFS traversal of the vertices
+	// reachable from v
+	void DFS(int v);
+};
+
+void Graph::addEdge(int v, int w)
+{
+	// Add w to v’s list.
+	adj[v].push_back(w);
+    adj[w].push_back(v);
+}
+
+void Graph::DFS(int v)
+{
+	// Mark the current node as visited and
+	// print it
+	visited[v] = true;
+	cout << v << " ";
+
+	// Recur for all the vertices adjacent
+	// to this vertex
+	list<int>::iterator i;
+	for (i = adj[v].begin(); i != adj[v].end(); ++i)
+		if (!visited[*i])
+			DFS(*i);
+}
+
+// Driver code
+int main()
+{
+	// Create a graph given in the above diagram
+	Graph g;
+	g.addEdge(0, 1);
+	g.addEdge(0, 2);
+	g.addEdge(1, 2);
+	g.addEdge(2, 3);
+	g.addEdge(3, 3);
+
+	cout << "Following is Depth First Traversal"
+			" (starting from vertex 1) \n";
+
+	// Function call
+	g.DFS(1);
+
+	return 0;
+}
